ab0d3467bb3075507de29216ea755eb4b4fe0c0d
lafenicecc <ck-cc@hotmail.com>
2017-07-21 16:48:50 +0800

    Add channel operations

--- restserver/resources/api.py
+++ restserver/resources/api.py
@@ -11,6 +11,32 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
 ops_rest = Blueprint('ops_rest', __name__,
                      url_prefix='/{}'.format("api"))
 
+
 @ops_rest.route('/create_channel', methods=['GET'])
 def create_channel():
-    return make_ok_resp(data={})
+    cmd = "peer channel create -o orderer.example.com:7050 -c businesschannel -f ./channel-artifacts/channel.tx"
+    status, out = commands.getstatusoutput(cmd=cmd)
+    if status != 0:
+        return make_fail_resp(error="peer channel create failed",
+                              data={"msg": out})
+    return make_ok_resp(data={"msg": out})
+
+
+@ops_rest.route('/join_channel', methods=['GET'])
+def join_channel():
+    cmd = "peer channel join -b businesschannel.block -o orderer.example.com:7050"
+    status, out = commands.getstatusoutput(cmd=cmd)
+    if status != 0:
+        return make_fail_resp(error="peer channel join failed",
+                              data={"msg": out})
+    return make_ok_resp(data={"msg": out})
+
+
+@ops_rest.route('/update_anchor_peers', methods=['GET'])
+def update_anchor_peers():
+    cmd = "peer channel create -o orderer.example.com:7050 -c businesschannel -f ./channel-artifacts/Org1MSPanchors.tx"
+    status, out = commands.getstatusoutput(cmd=cmd)
+    if status != 0:
+        return make_fail_resp(error="peer channel create failed",
+                              data={"msg": out})
+    return make_ok_resp(data={"msg": out})
