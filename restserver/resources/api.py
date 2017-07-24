e87ce59734907d44887344027b95a6fe36c3824e
lafenicecc <ck-cc@hotmail.com>
2017-07-24 14:34:08 +0800

    Add chaincode upgrade and mount chaincode folder

--- restserver/resources/api.py
+++ restserver/resources/api.py
@@ -7,6 +7,7 @@ from flask import request as r
 
 from common import make_ok_resp, make_fail_resp
 
+
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
 
 ops_rest = Blueprint('ops_rest', __name__,
@@ -68,8 +69,7 @@ def install_chaincode():
         return make_fail_resp(error=error_msg, data=r.form)
 
     cmd = "peer chaincode install " \
-          + "-n {0} -v {1} -p {2} ".format(name, version, path) \
-          + "-o orderer.example.com:7050"
+          + "-n {0} -v {1} -p {2} ".format(name, version, path)
     status, out = commands.getstatusoutput(cmd=cmd)
     if status != 0:
         return make_fail_resp(error="peer chaincode install failed",
@@ -101,6 +101,33 @@ def instantiate_chaincode():
     status, out = commands.getstatusoutput(cmd=cmd)
     if status != 0:
         return make_fail_resp(error="peer chaincode instantiate failed",
+                              data={"msg": out})
+    return make_ok_resp(data={"msg": out})
+
+
+@ops_rest.route('/upgrade_chaincode', methods=['POST'])
+def upgrade_chaincode():
+    request_data = r.get_json(force=True, silent=True)
+    if r.form:
+        name = r.form["name"]
+        version = r.form["version"]
+        ctor = r.form["ctor"]
+    else:
+        name = request_data.get("name")
+        version = request_data.get("version")
+        ctor = request_data.get("ctor")
+
+    if not name or not version or not ctor:
+        error_msg = "not enough arguments"
+        return make_fail_resp(error=error_msg, data=r.form)
+
+    cmd = "peer chaincode upgrade " \
+          + "-o orderer.example.com:7050 " \
+          + "-C businesschannel " \
+          + "-n {0} -v {1} -c {2} ".format(name, version, ctor)
+    status, out = commands.getstatusoutput(cmd=cmd)
+    if status != 0:
+        return make_fail_resp(error="peer chaincode upgrade failed",
                               data={"msg": out})
     return make_ok_resp(data={"msg": out})
 
